607283a316f337cfd3b860aa01929fc07a59de77
Peyton Sonnefeld <peyton.sonnefeld@gmail.com>
2021-04-19 17:48:53 -0600

    test

--- index.js
+++ index.js
@@ -247,7 +247,7 @@ bot.on("message", async (message) => {
   }
   }
 
-  if (lowerCase.includes("well") && lowerCase.includes("hi") || lowerCase.includes("whale") && lowerCase.includes("hai")) {
+  if (lowerCase.includes("well hi") || lowerCase.includes("whale") && lowerCase.includes("hai")) {
     message.channel.send("https://www.youtube.com/watch?v=Z-yJTjbswhw")
   }
 
@@ -259,7 +259,7 @@ bot.on("message", async (message) => {
     }
   }
 
-  if (lowerCase.includes("german")|| lowerCase.includes("tinfoil") || lowerCase.includes("conspiracy")) {
+  if (lowerCase.includes("german")|| lowerCase.includes(prefix + "tinfoil") || lowerCase.includes("conspiracy")) {
     if (message.author.bot) {
       return;
     } else {
@@ -280,6 +280,127 @@ bot.on("message", async (message) => {
       return;
     } else {
       message.channel.send("https://www.youtube.com/watch?v=bKgWm5TNeBA")
+    }
+  }
+
+  if (lowerCase.includes(prefix + "grind")) {
+    if (message.author.bot) {
+      return;
+    } else {
+      message.channel.send({files: ["./Assets/grind.gif"]})
+    }
+  }
+
+  if (lowerCase.includes(prefix + "kinky")) {
+    if (message.author.bot) {
+      return;
+    } else {
+      message.channel.send({files: ["./Assets/kink.gif"]})
+    }
+  }
+
+  if (lowerCase.includes(prefix + "goodnight")) {
+    if (message.author.bot) {
+      return;
+    } else {
+      message.channel.send("Goodnight, sir.")
+      message.channel.send({files: ["./Assets/KO.gif"]})
+    }
+  }
+
+  if (lowerCase.includes(prefix + "flipflop")) {
+    if (message.author.bot) {
+      return;
+    } else {
+      message.channel.send({files: ["./Assets/flipflop.gif"]})
+    }
+  }
+
+  if (lowerCase.includes(prefix + "beans")) {
+    if (message.author.bot) {
+      return;
+    } else {
+      message.channel.send("https://youtu.be/bM25p7tGcOQ")
+    }
+  }
+
+  if (lowerCase.includes(prefix + "gat")) {
+    if (message.author.bot) {
+      return;
+    } else {
+      message.channel.send({files: ["./Assets/hkgun.png"]})
+    }
+  }
+
+  if (lowerCase.includes(prefix + "banana")) {
+    if (message.author.bot) {
+      return;
+    } else {
+      message.channel.send("https://www.youtube.com/watch?v=S-Eyhq-D9yM")
+    }
+  }
+
+  if (lowerCase.includes(prefix + "paradise")) {
+    if (message.author.bot) {
+      return;
+    } else {
+      message.channel.send(`I was shooting heroin and reading “The Fountainhead” in the front seat of my privately owned police cruiser when a call came in. I put a quarter in the radio to activate it. It was the chief.
+
+“Bad news, detective. We got a situation.”
+
+“What? Is the mayor trying to ban trans fats again?”
+
+“Worse. Somebody just stole four hundred and forty-seven million dollars’ worth of bitcoins.”
+
+The heroin needle practically fell out of my arm. “What kind of monster would do something like that? Bitcoins are the ultimate currency: virtual, anonymous, stateless. They represent true economic freedom, not subject to arbitrary manipulation by any government. Do we have any leads?”
+
+“Not yet. But mark my words: we’re going to figure out who did this and we’re going to take them down … provided someone pays us a fair market rate to do so.”
+
+“Easy, chief,” I said. “Any rate the market offers is, by definition, fair.”
+
+He laughed. “That’s why you’re the best I got, Lisowski. Now you get out there and find those bitcoins.”
+
+“Don’t worry,” I said. “I’m on it.”`)
+      message.channel.send(`I put a quarter in the siren. Ten minutes later, I was on the scene. It was a normal office building, strangled on all sides by public sidewalks. I hopped over them and went inside.
+
+“Home Depot™ Presents the Police!®” I said, flashing my badge and my gun and a small picture of Ron Paul. “Nobody move unless you want to!” They didn’t.
+
+“Now, which one of you punks is going to pay me to investigate this crime?” No one spoke up.
+
+“Come on,” I said. “Don’t you all understand that the protection of private property is the foundation of all personal liberty?”
+
+It didn’t seem like they did.
+
+“Seriously, guys. Without a strong economic motivator, I’m just going to stand here and not solve this case. Cash is fine, but I prefer being paid in gold bullion or autographed Penn Jillette posters.”
+
+Nothing. These people were stonewalling me. It almost seemed like they didn’t care that a fortune in computer money invented to buy drugs was missing.
+
+I figured I could wait them out. I lit several cigarettes indoors. A pregnant lady coughed, and I told her that secondhand smoke is a myth. Just then, a man in glasses made a break for it.
+
+“Subway™ Eat Fresh and Freeze, Scumbag!®” I yelled.
+
+Too late. He was already out the front door. I went after him.`)
+      message.channel.send(`“Stop right there!” I yelled as I ran. He was faster than me because I always try to avoid stepping on public sidewalks. Our country needs a private-sidewalk voucher system, but, thanks to the incestuous interplay between our corrupt federal government and the public-sidewalk lobby, it will never happen.
+
+I was losing him. “Listen, I’ll pay you to stop!” I yelled. “What would you consider an appropriate price point for stopping? I’ll offer you a thirteenth of an ounce of gold and a gently worn ‘Bob Barr ‘08’ extra-large long-sleeved men’s T-shirt!”
+
+He turned. In his hand was a revolver that the Constitution said he had every right to own. He fired at me and missed. I pulled my own gun, put a quarter in it, and fired back. The bullet lodged in a U.S.P.S. mailbox less than a foot from his head. I shot the mailbox again, on purpose.
+
+“All right, all right!” the man yelled, throwing down his weapon. “I give up, cop! I confess: I took the bitcoins.”
+
+“Why’d you do it?” I asked, as I slapped a pair of Oikos™ Greek Yogurt Presents Handcuffs® on the guy.
+
+“Because I was afraid.”
+
+“Afraid?”
+
+“Afraid of an economic future free from the pernicious meddling of central bankers,” he said. “I’m a central banker.”
+
+I wanted to coldcock the guy. Years ago, a central banker killed my partner. Instead, I shook my head.
+
+“Let this be a message to all your central-banker friends out on the street,” I said. “No matter how many bitcoins you steal, you’ll never take away the dream of an open society based on the principles of personal and economic freedom.”
+
+He nodded, because he knew I was right. Then he swiped his credit card to pay me for arresting him.`)
     }
   }
 
