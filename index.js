c741b438a102e3f17a38ff69f01f595b3155abfe
Peyton Sonnefeld <peyton.sonnefeld@gmail.com>
2021-04-20 17:49:34 -0600

    test

--- index.js
+++ index.js
@@ -432,11 +432,11 @@ He nodded, because he knew I was right. Then he swiped his credit card to pay me
     lowerCase.split(" ")
     for (var i in lowerCase) {
       if (lowerCase[i] == "shit") {
-        lowerCase[i] = "poop"
+        lowerCase[i] = "*poop*"
       }
     }
     lc = lowerCase
-    message.channel.send(lc)
+    message.channel.send("Don't you  mean" + lc + "?")
   }
 
   if (lowerCase === prefix + "hot")
