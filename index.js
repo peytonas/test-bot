709303965740fae7668287c9f110249a7a3e13ed
Peyton Sonnefeld <53791538+peytonas@users.noreply.github.com>
2021-05-15 21:22:32 -0600

    Update index.js

--- index.js
+++ index.js
@@ -899,7 +899,8 @@ He nodded, because he knew I was right. Then he swiped his credit card to pay me
       let y = getRandomInt(30)
       if (y < 2) {
         message.channel.send("https://www.youtube.com/watch?v=QrGrOK8oZG8")
-      } 
+      } else {
+        message.channel.send({files: ["./Assets/yikes.gif"]});
     }else {
         message.channel.send({ files: ["./Assets/yikes.gif"] });
       }
