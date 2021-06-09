a143afacad94c359fd02a12b8025354fea1ba06d
Peyton Sonnefeld <peyton.sonnefeld@gmail.com>
2021-06-09 16:04:16 -0600

    fix

--- index.js
+++ index.js
@@ -404,10 +404,10 @@ bot.on("message", async (message) => {
       }, 5000);
       setTimeout(function () {
         message.channel.send("The Dark Side of the Force is a pathway to many abilities some consider to be unnatural.")
-      }, 5000);
+      }, 10000);
       setTimeout(function () {
         message.channel.send("He became so powerful the only thing he was afraid of was losing his power, which eventually, of course, he did. Unfortunately, he taught his apprentice everything he knew, then his apprentice killed him in his sleep. Ironic. He could save others from death, but not himself.")
-      }, 5000);
+      }, 15000);
     }
   }
 
