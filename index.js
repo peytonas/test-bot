7211fb932b9d23296f4f3f776f190002289fa8f7
Peyton Sonnefeld <peyton.sonnefeld@gmail.com>
2021-06-09 15:51:05 -0600

    test

--- index.js
+++ index.js
@@ -166,12 +166,9 @@ let grossGifs = [
 ];
 
 let uncomfortableReactions = [
-  //"ಠಠ", 
-  // "◉_◉",
-  // "⚆ _ ⚆",
-  "./Pics/lickitung.png",
-  "./Pics/mime.png",
-  "./Pics/duck.png"
+  "ಠಠ", 
+  "◉_◉",
+  "⚆ _ ⚆"
 ];
 
 let pokePics = ["./Pics/sirBulba.png", "./Pics/sirChar.png", "./Pics/sirSquirt.png", "./Pics/sirPika.png"]
@@ -394,6 +391,31 @@ bot.on("message", async (message) => {
       return;
     } else {
       message.channel.send({files: ["./Assets/smarf.gif"]})
+    }
+  }
+
+  if (lowerCase.includes(prefix + "tragedy")) {
+    if (message.author.bot) {
+      return;
+    } else {
+      message.channel.send("Did you ever hear the tragedy of Darth Plagueis The Wise?")
+      setTimeout(function () {
+        message.channel.send("I thought not. It's not a story the Jedi would tell you. It's a Sith legend. Darth Plagueis was a Dark Lord of the Sith, so powerful and so wise he could use the Force to influence the midichlorians to create life... He had such a knowledge of the Dark Side that he could even keep the ones he cared about from dying.")
+      }, 5000);
+      setTimeout(function () {
+        message.channel.send("The Dark Side of the Force is a pathway to many abilities some consider to be unnatural.")
+      }, 5000);
+      setTimeout(function () {
+        message.channel.send("He became so powerful the only thing he was afraid of was losing his power, which eventually, of course, he did.Unfortunately, he taught his apprentice everything he knew, then his apprentice killed him in his sleep. Ironic. He could save others from death, but not himself.")
+      }, 5000);
+    }
+  }
+
+  if (lowerCase === "is it possible to learn this power?") {
+    if (message.author.bot) {
+      return;
+    } else {
+      message.channel.send("Not from a Jedi...")
     }
   }
 
