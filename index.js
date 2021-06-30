fb88a5ddd52181d51e2116282eaa0db8128ca5c2
Peyton Sonnefeld <peyton.sonnefeld@gmail.com>
2021-06-29 21:39:01 -0600

    test

--- index.js
+++ index.js
@@ -261,7 +261,8 @@ bot.on("message", async (message) => {
       return;
     } else {
       let i = getRandomInt(20);
-      message.reply(i);
+      i == 0 ? i++ : null;
+      message.channel.send("You rolled a...", i);
     }
   }
 
